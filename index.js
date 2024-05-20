f6ec001085c0c7201cab7f6566a8f4074b9d886b
Satyanarayana Uppada <98538331+satya-uppada@users.noreply.github.com>
2024-05-20 20:05:43 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-const http = require('http');
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.json());
-app.use(express.static("express"));
-// default URL for website
-app.use('/', function(req,res){
-    res.sendFile(path.join(__dirname+'/express/index.html'));
-    //__dirname : It will resolve to your project folder.
-  });
-const server = http.createServer(app);
-const port = process.env.PORT;
-server.listen(port);
-console.debug('Server listening on port ' + port);
+const express = require('express')
+const path = require('path')
+
+const PORT = process.env.PORT || 5001
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
